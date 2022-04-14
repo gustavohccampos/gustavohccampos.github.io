d9ff832bdd9b1bf026fef22a947c493639219cae
Gustavo Henrique Carvalho Campos <53590418+gustavohccampos@users.noreply.github.com>
2022-04-14 18:27:42 -0300

    ";"

--- Jogo_Forca_JS/main.js
+++ Jogo_Forca_JS/main.js
@@ -1,18 +1,18 @@
 //contador de erros
-let erro = 0
+let erro = 0;
 
 //validar se jogo esta iniciado
-let iniciado = 0
+let iniciado = 0;
 
 //pegar a palavra
-var palavra = ''
-var palavraIngles = ''
+var palavra = '';
+var palavraIngles = '';
 
 //contar quantidade de letras da palavra
-var count = 0
+var count = 0;
 
 //transformar a palavra em array
-var parray = {}
+var parray = {};
 
 //LER a Tecla digitada
 document.onkeypress = function (e) {
@@ -24,64 +24,63 @@ document.onkeypress = function (e) {
       document.getElementById('mensagemOk').innerHTML =
         '<p>Parabéns! Você Ganhou!!!Acertou a palavra: ' +
         palavraIngles.toUpperCase() +
-        ' </p>'
+        ' </p>';
     }
   }
 }
 
 //Função para capturar a tecla digitada
 function keyPressed(e) {
-  e = e || window.event
-  var key = e.keyCode || e.which
-  return String.fromCharCode(key)
+  e = e || window.event;
+  var key = e.keyCode || e.which;
+  return String.fromCharCode(key);
 }
 
 //funcao reiniciar o jogo
 function reiniciar() {
   for (var i = 0; i < parray.length; i++) {
     var elemento = document.querySelector('#letra' + i)
-    elemento.parentNode.removeChild(elemento)
+    elemento.parentNode.removeChild(elemento);
   }
-  palavra = ''
-  palavraIngles = ''
-  parray = []
-  count = 0
-  erro = 0
-  iniciado = 1
-  document.getElementById('forca').innerHTML = '<img src="img/forca.png" />'
-  document.getElementById('mensagemFim').textContent = ''
-  document.getElementById('mensagemOk').textContent = ''
+  palavra = '';
+  palavraIngles = '';
+  parray = [];
+  count = 0;
+  erro = 0;
+  iniciado = 1;
+  document.getElementById('forca').innerHTML = '<img src="img/forca.png" />';
+  document.getElementById('mensagemFim').textContent = '';
+  document.getElementById('mensagemOk').textContent = '';
 
-  fetch('teste.json')
+  fetch('palavras.txt')
     .then(response => response.text())
     .then(text => {
-      const array = text.split('\r\n')
-      let tamanho = array.length
+      const array = text.split('\r\n');
+      let tamanho = array.length;
 
-      var aleatorio = ~~(Math.random() * tamanho)
+      var aleatorio = ~~(Math.random() * tamanho);
 
-      let index = array[aleatorio].indexOf(',')
+      let index = array[aleatorio].indexOf(',');
 
       document.getElementById('palavra').innerHTML =
         '<div id="palavra">A palavra é: <p>' +
         array[aleatorio].split(',', 1) +
-        '</p>'
-      ;('</div>')
+        '</p></div>';
 
-      palavraIngles = array[aleatorio].substring(index + 1)
-      parray = palavraIngles.split('')
-      count = parray.length
-      palavra = array[aleatorio].substring(0, index)
+      palavraIngles = array[aleatorio].substring(index + 1);
+      parray = palavraIngles.split('');
+      count = parray.length;
+      palavra = array[aleatorio].substring(0, index);
 
       //monta o campo das letras para preencher
-      contarLetras(palavraIngles, count, parray)
+      contarLetras(palavraIngles, count, parray);
     })
 }
 
 //funcao para contar as letras que a palavra possui
 function contarLetras(palavra, count, parray) {
   for (var i = 0; i < count; i++) {
-    letras.innerHTML += "<li id='letra" + i + "'>_</li>"
+    letras.innerHTML += "<li id='letra" + i + "'>_</li>";
   }
 }
 
@@ -89,31 +88,31 @@ function contarLetras(palavra, count, parray) {
 function tecla(tecla, palavra, count, parray) {
   for (var i = 0; i < palavra.length; i++) {
     if (parray[i] == tecla) {
-      document.getElementById('letra' + i).innerHTML = tecla
+      document.getElementById('letra' + i).innerHTML = tecla;
     }
   }
 
   if (!palavra.includes(tecla)) {
-    erro++
+    erro++;
     document.getElementById('forca').innerHTML =
-      '<img src="img/forca' + erro + '.png" />'
+      '<img src="img/forca' + erro + '.png" />';
     if (erro == 5) {
       document.getElementById('mensagemFim').innerHTML =
         '<p>Que pena, Fim de Jogo!Tente outra vez</p><p>A palavra correta é: ' +
         palavraIngles.toUpperCase() +
-        '</p>'
+        '</p>';
     }
   }
 }
 
 //funcao para checkar as letras digitadas e conferir com a palavra final
 function checkLetras(parray, palavra) {
-  let juntarLetras = ''
+  let juntarLetras = '';
   for (var i = 0; i < parray.length; i++) {
-    juntarLetras += document.getElementById('letra' + i).textContent
+    juntarLetras += document.getElementById('letra' + i).textContent;
   }
   if (juntarLetras == palavra) {
-    document.getElementById('forca').innerHTML = '<img src="img/fim.png" />'
-    return true
-  } else return false
+    document.getElementById('forca').innerHTML = '<img src="img/fim.png" />';
+    return true;
+  } else return false;
 }
